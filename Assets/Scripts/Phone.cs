7efe3cdce3b114d5b84fb01d69a0c83ffad3695d
J3rry <jeremydix17@gmail.com>
2024-07-01 03:30:00 +0200

    added phone transition

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using FMOD.Studio;
 using FMODUnity;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +15,14 @@ public class Phone : MonoBehaviour
     [SerializeField] private float positionTurnSpeed;
     [SerializeField] private float rotationTurnSpeed;
     [SerializeField] private EventInstance callInstance;
+
+    [SerializeField] private float transitionDistanceStart;
+    [SerializeField] private float transitionDistanceEnd;
+    
+    private float configuredDistance;
+    private float scaledDistance;
+    private float phonePlayerDistance;
+    private GameObject player;
 
 
     [SerializeField] private EventReference callSound;
@@ -34,6 +43,17 @@ public class Phone : MonoBehaviour
         inputActions = GameObject.Find("Player").GetComponent<PlayerMovement>().inputActions;
         callInstance = RuntimeManager.CreateInstance(callSound);
         callInstance.set3DAttributes(transform.To3DAttributes());
+        player = GameObject.Find("Player");
+        
+        //start 5
+        //end 1
+        //configuredDistance = 4
+        //phonePlayerDistance = 2
+        //map configuredDistance to 0-1
+        
+        configuredDistance = transitionDistanceStart - transitionDistanceEnd;
+        phonePlayerDistance = Vector3.Distance(transform.position, player.transform.position) - transitionDistanceEnd;
+        scaledDistance = 1 - ((phonePlayerDistance - transitionDistanceEnd) / configuredDistance);
         
         StartRing(); //nochekin
     }
@@ -58,7 +78,10 @@ public class Phone : MonoBehaviour
 
     private void Update()
     {
-
+        phonePlayerDistance = Vector3.Distance(transform.position, player.transform.position) - transitionDistanceEnd;
+        scaledDistance = 1 - ((phonePlayerDistance - transitionDistanceEnd) / configuredDistance);
+        callInstance.setParameterByName("RingParameter", scaledDistance);
+        
         if (isMoving && !callIsActive && Vector3.Distance(phoneHandle.transform.position, phoneHoldTransform.position) < 0.005f)
         {
             if (isCalling)
