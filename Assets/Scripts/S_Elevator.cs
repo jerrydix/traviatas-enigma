ca8f1feea9ea93461a5ae7d7707781248a8b4da1
MichailZh <37882050+MichailZh@users.noreply.github.com>
2024-07-03 01:54:27 +0200

    Lift

--- Assets/Scripts/S_Elevator.cs
+++ Assets/Scripts/S_Elevator.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class S_Elevator : MonoBehaviour
 {
@@ -11,7 +14,10 @@ public class S_Elevator : MonoBehaviour
     private bool isMoving;
     private S_Effects effects;
     [SerializeField] private bool intro;
+    [SerializeField] private EventReference[] LiftSounds;
     private GameObject player;
+    private EventInstance musicInstance;
+    private bool isIn;
     public Vector3 teleportPosition = new Vector3(0f, 1f, 0f);
     
     [Header("Buttons")]
@@ -20,6 +26,7 @@ public class S_Elevator : MonoBehaviour
 
     private void Start()
     {
+        musicInstance = RuntimeManager.CreateInstance(LiftSounds[1]);
         anim = GetComponent<Animator>();
         player = GameObject.FindWithTag("Player");
         effects = player.GetComponent<S_Effects>();
@@ -29,18 +36,18 @@ public class S_Elevator : MonoBehaviour
     {
         if (isClosed)
         {
-            if (!isMoving && !intro)
+            if (!isMoving && !intro && isIn)
             {
                 StartCoroutine(Open());
             }
         }
         else
         {
-            if (!isMoving && !intro)
+            if (!isMoving && !intro && isIn)
             {
                 StartCoroutine(Close());
             }
-            else if (!isMoving && intro)
+            else if (!isMoving && intro && isIn)
             {
                 StartCoroutine(Close());
                 StartCoroutine(Blink());
@@ -52,18 +59,31 @@ public class S_Elevator : MonoBehaviour
     {
         if (isClosed)
         {
+            AudioManager.Instance.PlayOneShot(LiftSounds[0], transform.position);
             outsideButton.PressButton(positionTurnSpeed);
             anim.Play("Open");
             isClosed = false;
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        isIn = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isIn = false;
+    }
+
     IEnumerator Close()
     {
         isMoving = true;
         insideButton.PressButton(positionTurnSpeed);
+        AudioManager.Instance.PlayOneShot(LiftSounds[0], transform.position);
         anim.Play("Close");
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
+        musicInstance.start();
         isMoving = false;
         isClosed = true;
     }
@@ -72,19 +92,35 @@ public class S_Elevator : MonoBehaviour
     {
         isMoving = true;
         insideButton.PressButton(positionTurnSpeed);
+        AudioManager.Instance.PlayOneShot(LiftSounds[0], transform.position);
         anim.Play("Open");
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         isMoving = false;
         isClosed = false;
     }
 
     IEnumerator Blink()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(4f);
         effects.CloseEyes();
         yield return new WaitForSeconds(effects.effectLength);
         player.transform.position = teleportPosition;
+        StartCoroutine(setMusicParam());
         effects.OpenEyes();
         yield return new WaitForSeconds(effects.effectLength);
+    }
+
+    IEnumerator setMusicParam()
+    {
+
+        float volume = 0;
+        while (volume < 1)
+        {
+            volume += 0.1f;
+            musicInstance.setParameterByName("LiftVolume", volume);
+            yield return new WaitForSeconds(0.3f);
+        }
+        musicInstance.stop(STOP_MODE.IMMEDIATE);
+        
     }
 }
