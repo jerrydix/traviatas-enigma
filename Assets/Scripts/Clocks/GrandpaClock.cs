65dd6d39b650a2db3f23cbd896f58826f11ded75
J3rry <jeremydix17@gmail.com>
2024-07-06 01:17:36 +0200

    Clock fmod logic

---
+++ Assets/Scripts/Clocks/GrandpaClock.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandpaClock : MonoBehaviour
+{
+    [SerializeField] private string hour;
+    [SerializeField] private string minute; //not too many
+    
+    
+    
+}
